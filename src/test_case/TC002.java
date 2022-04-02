511e10edfc6e75bccd203f241d6df964c85e3b28
Mansur Nur <Mkalif1994@gmail.com>
2022-04-02 12:03:02 -0700

    Added positive testing for Tc001.java

---
+++ src/test_case/TC002.java
@@ -0,0 +1,15 @@
+package test_case;
+
+public class TC002 {
+    public static void main(String[] args) {
+        //test case 2
+
+        //writing report
+
+        //screenshot failure
+
+        //create smoking file
+    }
+
+
+}
